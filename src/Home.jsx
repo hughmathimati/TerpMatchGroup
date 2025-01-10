1313530ae22087dff67ee3146a8fa0cc64df8384
hughmathimati <102939561+hughmathimati@users.noreply.github.com>
2025-01-10 04:27:42 +0000

    Drafted up navigation sidebar.

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { useState, useEffect, useRef } from "react";
+import { BrowserRouter, NavLink } from "react-router";
+import './Home.css';
+
+function Home() {
+  return (<>
+    
+    </>
+  );
+}
+
+export default Home;
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router";
+import { useState, useEffect, useRef } from "react";
+import { BrowserRouter, NavLink } from "react-router";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+
+function Sidebar() {
+  const [open, setOpen] = useState(true);
+  //If you have any unread messages, they show up in here? In that case, we'd append those chats here.
+  var Menus = [
+    { title: "Profile", path: "profile", gap: true },
+    { title: "Home", path: "home" },
+    { title: "Browse Groups", path: "browse-groups" },
+  ];
+  // <img src={"/the folder/${Menu.title}.file format of the image"} />
+  // name image file accordingly
+  return (
+    <ul>
+      {Menus.map((Menu) => (<li>
+          <img />
+          <NavLink to={Menu.path} id="link">{Menu.title}</NavLink>
+          {Menu.gap? <p style={{"margin-bottom": "30px"}} /> : <></>}
+      </li>))}
+    </ul>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Sidebar />
+        
+      </BrowserRouter>
   </React.StrictMode>
 );
 
