2767c33fbb9dd9f604d2fdac5f8e43c03ff1d9f0
hughmathimati <102939561+hughmathimati@users.noreply.github.com>
2025-01-16 19:21:43 +0000

    Minor debugging and fixing. Swapped around Account (used to be called Profile) and Home pages. Changed the Browse Groups grid to be a flex container.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,8 +3,32 @@ import { useState, useEffect, useRef } from "react";
 import './Home.css';
 
 export default function Home() {
-  return (<>
-    <h1>Home</h1>
-    
-  </>);
+  return (
+    <div className="profile-page">
+      <header>
+        <h1>Home</h1>
+      </header>
+      <section className="current-courses">
+        <label>Current Courses</label>
+        <div className="circles">
+          {[...Array(7)].map((_, index) => (
+            <div key={index} className="circle"></div>
+          ))}
+        </div>
+        <div className="labels">
+          {[...Array(7)].map((_, index) => (
+            <label key={index}>Label</label>
+          ))}
+        </div>
+      </section>
+      <section className="blank-section">
+        <h2>Blank</h2>
+        <div className="squares">
+          {[...Array(5)].map((_, index) => (
+            <div key={index} className="square"></div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
