070743a984debf0ef58713e39739206fd8cea3d3
Eniola <145488741+eniolaloba@users.noreply.github.com>
2025-01-16 18:18:55 +0000

    added circles for the current class list

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,6 +5,24 @@ import './Home.css';
 export default function Home() {
   return (<>
     <h1>Home</h1>
+
+    <h2>Your Profile →</h2>
+
+    <h2>Current Classes →</h2>
+
+    <div className="classList">
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+    <div className="circle"></div>
+
+
+    </div>
+
+
     
   </>);
 }
