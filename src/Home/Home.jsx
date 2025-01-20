6739636e0764dafe147ac016ef0a5c6ae3623fd4
Eniola <145488741+eniolaloba@users.noreply.github.com>
2025-01-20 16:07:48 +0000

    updated the match and current class section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,35 +6,107 @@ export default function Home() {
   return (<>
     <h1>Home</h1>
 
-    <h2>Your Profile →</h2>
+    <h2>Your Profile () </h2>
 
     <h2>Current Classes →</h2>
-
     <div className="classList">
+
+    <div className="circleText">
     <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
+
+    <div className="circleText">
     <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
+
+    <div className="circleText">
     <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
+
+    <div className="circleText">
     <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
+
+    <div className="circleText">
     <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
+
+    <div className="circleText">
     <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
+
+    <div className="circleText">
     <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
+
+    <div className="circleText">
+    <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
+
+    <div className="circleText">
+    <div className="circle"></div>
+    <span className="label">Class</span>
+    </div>
   </div>
 
     <h2>Matches→</h2>
 
     <div className="matchList">
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+
+      <div className="squareText">
       <div className="square"></div>
+      <span className="label">Match</span>
+      </div>
+      
     </div>
 
-
-    
   </>);
 }
